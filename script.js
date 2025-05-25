70428b3b2c7847a2fd05ac00515de2feab99d94e
Mandy Jendroska <soulfreelancer@yahoo.com>
2025-05-26 00:02:17 +0200

    choose dom elememnts

---
+++ script.js
@@ -0,0 +1,27 @@
+const decisionInput = document.getElementById('decisionInput');
+const setDecisionBtn = document.getElementById('setDecisionBtn');
+const currentDecisionDisplay = document.getElementById('currentDecision');
+
+const argumentText = document.getElementById('argumentText');
+const addProBtn = document.getElementById('addProBtn');
+const addContraBtn = document.getElementById('addContraBtn');
+
+const proList = document.getElementById('proList');
+const contraList = document.getElementById('contraList');
+const proCountSpan = document.getElementById('proCount');
+const contraCountSpan = document.getElementById('contraCount');
+
+const proScoreSpan = document.getElementById('proScore');
+const contraScoreSpan = document.getElementById('contraScore');
+const overallRecommendationSpan = document.getElementById('overallRecommendation');
+
+const startReflectionBtn = document.getElementById('startReflectionBtn');
+const showSummaryBtn = document.getElementById('showSummaryBtn');
+
+const agentChat = document.getElementById('agentChat');
+const reflectionInputSection = document.getElementById('reflectionInputSection');
+const reflectionInput = document.getElementById('reflectionInput');
+const submitReflectionBtn = document.getElementById('submitReflectionBtn');
+const speechToggle = document.getElementById('speechToggle');
+
+
